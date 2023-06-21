b2215feaf39e0b68eaaa2d7cfebe5fdd015806f5
AP <aleksandrpolishchuk@gmail.com>
2023-06-21 23:59:27 +0700

    add navbar component

--- frontend/ticketier-front-nextjs/components/navbar/Navbar.tsx
+++ frontend/ticketier-front-nextjs/components/navbar/Navbar.tsx
@@ -1,4 +1,11 @@
-import React from "react";
+import Link from "next/link";
+import { AiOutlineHome } from "react-icons/ai";
+import { MdAirplaneTicket } from "react-icons/md";
+
+const menuItems = [
+  { id: 1, label: "Home", route: "/", icon: <AiOutlineHome /> },
+  { id: 2, label: "Tickets", route: "/tickets", icon: <AiOutlineHome /> },
+];
 
 const Navbar = () => {
   return <div>Navbar</div>;
