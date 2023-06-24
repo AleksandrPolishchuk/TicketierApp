143daf2c9a0b8c15194fdb36534ded3f68791611
AP <aleksandrpolishchuk@gmail.com>
2023-06-24 18:58:37 +0700

    add BackButton component(frontend)

--- frontend/ticketier-front-nextjs/components/back-button/BackButton.tsx
+++ frontend/ticketier-front-nextjs/components/back-button/BackButton.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
 import React from "react";
+import { BsArrowLeft } from "react-icons/bs";
 
-const BackButton = () => {
-  return <div>BackButton</div>;
+interface Props {
+  destination?: string;
+}
+
+const BackButton = ({ destination = "/" }: Props) => {
+  return (
+    <div className="flex">
+      <Link
+        href={destination}
+        className="bg-sky-800 text-white px-4 py-1 rounded-lg w-fit"
+      >
+        <BsArrowLeft className="text-2xl" />
+      </Link>
+    </div>
+  );
 };
 
 export default BackButton;
