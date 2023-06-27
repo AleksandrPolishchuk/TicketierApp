7aa753b871e3830c4d717903c3f72812260beb9a
AP <aleksandrpolishchuk@gmail.com>
2023-06-28 00:30:22 +0700

    add CustomSpinner component (frontend)

--- frontend/ticketier-front-nextjs/components/custom-spinner/CustomSpinner.tsx
+++ frontend/ticketier-front-nextjs/components/custom-spinner/CustomSpinner.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const CustomSpinner = () => {
   return (
     <div className="h-48 flex justify-center items-center">
-      <span>
-        <span></span>
-        <span></span>
+      <span className="relative h-20 w-20">
+        <span className="bg-sky-600 absolute w-full h-full rounded-full blur-lg animate-ping"></span>
+        <span className="bg-sky-600 inline-flex h-20 w-20 rounded-full blur-xl"></span>
       </span>
     </div>
   );
