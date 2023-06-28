d39a3674361f7f02067d01143d094c33a65f5426
AP <aleksandrpolishchuk@gmail.com>
2023-06-28 23:31:15 +0700

    add Link to HomePage (frontend)

--- frontend/ticketier-front-nextjs/app/page.tsx
+++ frontend/ticketier-front-nextjs/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const HomePage = () => {
   return (
     <div className="pageGeneralClass flex flex-col justify-center items-center gap-y-8 sm:gap-y-16">
@@ -7,6 +9,12 @@ const HomePage = () => {
           <div className="absolute inset-0 w-full h-8 sm:h-12 rounded-full bg-gradient-to-t from-purple-500 to-blue-300 blur-md opacity-40"></div>
         </h1>
       </div>
+      <Link
+        href="/tickets"
+        className="bg-sky-800 text-white px-4 py-2 rounded-lg"
+      >
+        Tickets
+      </Link>
     </div>
   );
 };
