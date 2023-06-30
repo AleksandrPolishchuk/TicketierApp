b11402e6f1a936aadfad1a584a53ec3494ee501b
AP <aleksandrpolishchuk@gmail.com>
2023-06-30 18:07:26 +0700

    add styles for NotFound Page

--- frontend/ticketier-front-nextjs/app/not-found.tsx
+++ frontend/ticketier-front-nextjs/app/not-found.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className="pageGeneralClass">
+      <h1 className="text-red-500 text-2xl mt-8">Not Found Page</h1>
+    </div>
+  );
 };
 
 export default NotFoundPage;
