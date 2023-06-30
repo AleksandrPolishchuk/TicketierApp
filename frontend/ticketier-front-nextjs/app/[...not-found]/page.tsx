6c266314036076c37924daa2330be19c8cad926f
AP <aleksandrpolishchuk@gmail.com>
2023-06-30 18:20:14 +0700

    add img for NotFound Page (frontend)

--- frontend/ticketier-front-nextjs/app/[...not-found]/page.tsx
+++ frontend/ticketier-front-nextjs/app/[...not-found]/page.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Image from "next/image";
 
 const NotFoundPage = () => {
   return (
     <div className="pageGeneralClass">
-      <h1 className="text-red-500 text-2xl mt-8">Not Found Page</h1>
+      <div className="py-12 flex flex-col justify-center items-center">
+        <Image
+          src="/404.png"
+          width={300}
+          height={300}
+          alt="not-found"
+          loading="lazy"
+        />
+        <h1 className="text-red-500 text-2xl mt-8">Not Found Page</h1>
+      </div>
     </div>
   );
 };
