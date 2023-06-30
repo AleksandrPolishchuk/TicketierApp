a7b97d592c1496e6331b2156fa510cb0f7bbf28c
AP <aleksandrpolishchuk@gmail.com>
2023-06-30 18:40:48 +0700

    add fetch data from server (frontend)

--- frontend/ticketier-front-nextjs/app/tickets/page.tsx
+++ frontend/ticketier-front-nextjs/app/tickets/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-const TicketsListPage = () => {
+async function getTickets(url: string) {
+  // This is SSR
+  const res = await fetch(url, { cache: "no-store" });
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const TicketsListPage = async () => {
   const url = process.env.NEXT_PUBLIC_BASE_URL;
+
+  // Fetch data from url
+  const tickets = await getTickets(url);
+
+  console.log(tickets);
+
   return <div>TicketsListPage</div>;
 };
 
