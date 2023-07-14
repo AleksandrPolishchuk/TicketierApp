789be0f3061aa5e42ac2c14a6f1c00c6653846bc
AP <aleksandrpolishchuk@gmail.com>
2023-07-14 23:59:29 +0700

    add AiOutlineSearch to SearchBox component

--- frontend/ticketier-front-nextjs/app/tickets/components/SearchBox.tsx
+++ frontend/ticketier-front-nextjs/app/tickets/components/SearchBox.tsx
@@ -1,17 +1,18 @@
 "use client";
-
+import { AiOutlineSearch } from "react-icons/ai";
 import React, { useState, useEffect } from "react";
 
 const SearchBox = () => {
   const [searchValue, setSearchValue] = useState<string>("");
   return (
-    <div>
+    <div className="relative mb-4">
       <input
         type="text"
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
         className="outline-none border-2 border-gray-200 focus:border-gray-500 pl-8 py-1 rounded-lg w-full max-w;lg"
       />
+      <AiOutlineSearch className="absolute top-2 left-2 text-2xl text-gray-400" />
     </div>
   );
 };
