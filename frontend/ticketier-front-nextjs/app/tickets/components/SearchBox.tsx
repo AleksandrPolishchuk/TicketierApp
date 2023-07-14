3cf4317af6a6d16710b0a0026ba7039f6f01cb65
AP <aleksandrpolishchuk@gmail.com>
2023-07-14 23:47:27 +0700

    add jsx to SearchBox component

--- frontend/ticketier-front-nextjs/app/tickets/components/SearchBox.tsx
+++ frontend/ticketier-front-nextjs/app/tickets/components/SearchBox.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 
 const SearchBox = () => {
-  return <div>SearchBox</div>;
+  const [searchValue, setSearchValue] = useState<string>("");
+  return (
+    <div>
+      <input
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+    </div>
+  );
 };
 
 export default SearchBox;
