61c396ac2e1127c857a65e97d4c59d8b95023570
AP <aleksandrpolishchuk@gmail.com>
2023-07-18 23:15:06 +0700

    add hooks to CreateTicketForm

--- frontend/ticketier-front-nextjs/app/tickets/create/CreateTicketForm.tsx
+++ frontend/ticketier-front-nextjs/app/tickets/create/CreateTicketForm.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+"use client";
+import { TicketCreateDto } from "@/typings/generalTypes";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const CreateTicketForm = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const router = useRouter();
+
+  const form = useForm<TicketCreateDto>({});
+  const { register, handleSubmit, formState, reset } = form;
+  const { errors } = formState;
+
   return <div>CreateTicketForm</div>;
 };
 
