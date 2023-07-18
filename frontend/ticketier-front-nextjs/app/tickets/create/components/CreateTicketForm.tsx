ef51ab0aa033d5d464436708bb8f437881746946
AP <aleksandrpolishchuk@gmail.com>
2023-07-19 00:01:04 +0700

    add submit-btn to CreateTicketForm

--- frontend/ticketier-front-nextjs/app/tickets/create/components/CreateTicketForm.tsx
+++ frontend/ticketier-front-nextjs/app/tickets/create/components/CreateTicketForm.tsx
@@ -119,6 +119,12 @@ const CreateTicketForm = () => {
               {errors?.price?.message}
             </span>
           </div>
+
+          <div className="flex justify-between items-center">
+            <button className="bg-sky-800 text-white px-4 py-2 mx-8 my-2 rounded-lg">
+              Submit
+            </button>
+          </div>
         </form>
       </>
     </>
