eb9b5e13969e3472f80ada23f2bc4e4cb8d68801
AP <aleksandrpolishchuk@gmail.com>
2023-07-18 22:52:24 +0700

    modify jsx in TicketCreatePage

--- frontend/ticketier-front-nextjs/app/tickets/create/page.tsx
+++ frontend/ticketier-front-nextjs/app/tickets/create/page.tsx
@@ -1,7 +1,34 @@
+import BackButton from "@/components/back-button/BackButton";
+import BreadCrumb from "@/components/bread-crumb/BreadCrumb";
+import GeneralInnerTitle from "@/components/general-inner-title/GeneralInnerTitle";
+import Link from "next/link";
 import React from "react";
+import { AiOutlinePlusSquare } from "react-icons/ai";
 
 const TicketCreatePage = () => {
-  return <div>TicketCreatePage</div>;
+  return (
+    <div className="pageGeneralClass">
+      <div className="flex justify-start items-center gap-x-4">
+        <BackButton destination="/tickets" />
+        <BreadCrumb
+          items={[
+            {
+              label: "Tickets",
+              route: "/tickets",
+            },
+            {
+              label: "Create",
+              route: "/",
+            },
+          ]}
+        />
+      </div>
+
+      <div>
+        <GeneralInnerTitle title="Create Ticket" />
+      </div>
+    </div>
+  );
 };
 
 export default TicketCreatePage;
