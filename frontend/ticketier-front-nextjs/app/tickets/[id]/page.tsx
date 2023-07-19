20abd09aa0572fa06c0c238d4356e6993fef31ba
AP <aleksandrpolishchuk@gmail.com>
2023-07-19 23:56:43 +0700

    add new jsx to [id]/page

--- frontend/ticketier-front-nextjs/app/tickets/[id]/page.tsx
+++ frontend/ticketier-front-nextjs/app/tickets/[id]/page.tsx
@@ -1,7 +1,28 @@
+import BackButton from "@/components/back-button/BackButton";
+import BreadCrumb from "@/components/bread-crumb/BreadCrumb";
+import GeneralInnerTitle from "@/components/general-inner-title/GeneralInnerTitle";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="pageGeneralClass">
+      <div className="flex justify-start items-center gap-x-4">
+        <BackButton destination="/" />
+        <BreadCrumb
+          items={[
+            {
+              label: "Tickets",
+              route: "/tickets",
+            },
+          ]}
+        />
+      </div>
+
+      <div>
+        <GeneralInnerTitle title="Tickets List" />
+      </div>
+    </div>
+  );
 };
 
 export default page;
