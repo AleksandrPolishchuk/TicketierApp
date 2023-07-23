502493a1a424ea091f29385eed40e736a63ea2fa
AP <aleksandrpolishchuk@gmail.com>
2023-07-23 18:50:33 +0700

    add start jsx to TicketDetails component

--- frontend/ticketier-front-nextjs/app/tickets/[id]/components/TicketDetails.tsx
+++ frontend/ticketier-front-nextjs/app/tickets/[id]/components/TicketDetails.tsx
@@ -6,7 +6,14 @@ interface Props {
 }
 
 const TicketDetails = ({ ticket }: Props) => {
-  return <div>TicketDetails</div>;
+  return (
+    <div>
+      <div className="border-gray-100 border-2">
+        <span>Ticket Id</span>
+        <span>{ticket.id}</span>
+      </div>
+    </div>
+  );
 };
 
 export default TicketDetails;
