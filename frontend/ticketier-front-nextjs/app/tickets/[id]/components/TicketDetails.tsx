4e89d79834cd30fd1efc6c8dec28932919badbe5
AP <aleksandrpolishchuk@gmail.com>
2023-07-23 18:53:31 +0700

    add new styling for TicketDetails component

--- frontend/ticketier-front-nextjs/app/tickets/[id]/components/TicketDetails.tsx
+++ frontend/ticketier-front-nextjs/app/tickets/[id]/components/TicketDetails.tsx
@@ -7,10 +7,17 @@ interface Props {
 
 const TicketDetails = ({ ticket }: Props) => {
   return (
-    <div>
-      <div className="border-gray-100 border-2">
-        <span>Ticket Id</span>
-        <span>{ticket.id}</span>
+    <div className="border-2 border-gray-300 flex flex-col gap-y-4 p-4 rounded-lg">
+      <div>
+        <span className="text-lg font-bold text-sky-900 mr-4">Ticket Id</span>
+        <span className="text-lg">{ticket.id}</span>
+      </div>
+
+      <div>
+        <span className="text-lg font-bold text-sky-900 mr-4">
+          Pasenger Name
+        </span>
+        <span className="text-lg">{ticket.passengerName}</span>
       </div>
     </div>
   );
