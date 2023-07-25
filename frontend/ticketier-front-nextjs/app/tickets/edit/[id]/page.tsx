b5fa614e458ea9240aa30828b31ba30f64762057
AP <aleksandrpolishchuk@gmail.com>
2023-07-25 23:57:29 +0700

    add new jsx to TicketEditPage

--- frontend/ticketier-front-nextjs/app/tickets/edit/[id]/page.tsx
+++ frontend/ticketier-front-nextjs/app/tickets/edit/[id]/page.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import BackButton from "@/components/back-button/BackButton";
+import BreadCrumb from "@/components/bread-crumb/BreadCrumb";
+import GeneralInnerTitle from "@/components/general-inner-title/GeneralInnerTitle";
 
-const page = () => {
-  return <div>page</div>;
+const TicketEditPage = () => {
+  return (
+    <div className="pageGeneralClass">
+      <div className="flex justify-start items-center gap-x-4">
+        <BackButton destination="/tickets" />
+        <BreadCrumb
+          items={[
+            { label: "Tickets", route: "/tickets" },
+            { label: "Edit", route: "/" },
+          ]}
+        />
+      </div>
+
+      <div>
+        <GeneralInnerTitle title="Edit Ticket" />
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default TicketEditPage;
