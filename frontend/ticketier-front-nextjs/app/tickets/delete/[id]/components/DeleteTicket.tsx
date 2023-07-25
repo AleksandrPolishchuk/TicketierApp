d96518acee97a400d9f975afdc52751f319420e9
AP <aleksandrpolishchuk@gmail.com>
2023-07-26 01:48:13 +0700

    add jsx to DeleteTicket component

--- frontend/ticketier-front-nextjs/app/tickets/delete/[id]/components/DeleteTicket.tsx
+++ frontend/ticketier-front-nextjs/app/tickets/delete/[id]/components/DeleteTicket.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+"use client";
+import { Ticket } from "@/typings/generalTypes";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import CustomSpinner from "@/components/custom-spinner/CustomSpinner";
+import { MdOutlineDelete } from "react-icons/md";
+import axios from "axios";
+import toast from "react-hot-toast";
 
-const DeleteTicket = () => {
-  return <div>DeleteTicket</div>;
+interface Props {
+  ticket: Ticket;
+}
+
+const DeleteTicket = ({ ticket }: Props) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const router = useRouter();
+
+  const deleteHandlerFunction = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/delete/${ticket.id}`
+      );
+      router.push("/tickets");
+      router.refresh();
+      toast.success("Ticket deleted successfully");
+    } catch (error) {
+      // Handled
+      console.log(error);
+      toast.error("Error on deleting  Ticket");
+    }
+  };
+
+  return (
+    <>
+      {loading && <CustomSpinner />}
+      <>
+        <div
+          className={`border-2 border-red-200 w-96 max-w-full mx-auto mt-4 p-2 rounded-lg ${
+            loading ? "blur-xl" : ""
+          }`}
+        >
+          <h1 className="mx-4 my-2 text-red-600">
+            Are You sure you want to delete Ticket with this information?{" "}
+          </h1>
+        </div>
+      </>
+    </>
+  );
 };
 
 export default DeleteTicket;
